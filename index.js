34fefea411c96d82e6c08a3841a0b2eb440c5681
SMarquis85 <sophienathalie_85@outlook.com>
2023-07-15 14:09:39 -0400

    updated index, shapes and colorchoice js files

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const fs = require('fs');  // File system module
 const readlineSync = require('readline-sync');  // Synchronous user input module
 const text = require('./lib/text');  // Custom text module
+const shapes = require('./lib/shape');  // Custom shape module
 const basicTemplate = require('./lib/svgGenerator');  // Custom SVG template module
 
 // Function to generate SVG shapes
 function generateShapes(shape, shapeColor) {
   // Check the shape type and return the corresponding SVG code
+  return shapes.generateShapes(shape, shapeColor);
 }
 
 // Get user inputs for initials, character color, shape, and shape color
@@ -18,11 +20,12 @@ const shapeColor = readlineSync.question('What do you want the shape color to be
 // Function to handle user input for shape and shape color
 function shapeUserInput(shapeInput, shapeColorInput) {
   // Check if the shape is valid and return the corresponding SVG shape code
+  return shapes.generateShapes(shapeInput, shapeColorInput);
 }
 
 // Generate the SVG logo based on user inputs
 const textEl = text.getTextEl(initials);
-const colorTextEl = textEl.replace('white',. charColor);
+const colorTextEl = textEl.replace('white', charColor);
 const svgLogo = basicTemplate.writeBaseTemplate(shapeUserInput(shape, shapeColor), colorTextEl);
 
 // Output the generated SVG logo
